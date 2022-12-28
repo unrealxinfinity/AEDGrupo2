c929cd2f2c4db7124528c45683c406cc84f3c55d
Inês Soares <ines.soares.22102003@gmail.com>
2022-12-28 16:08:27 +0000

    Created Interface.h

---
+++ header/Interface.h
@@ -0,0 +1,28 @@
+//
+// Created by nesma on 28/12/2022.
+//
+
+#ifndef PROJETOGRUPO2_INTERFACE_H
+#define PROJETOGRUPO2_INTERFACE_H
+
+#include <queue>
+#include <string>
+#include <list>
+#include <math.h>
+#include "CSVReader.h"
+
+using namespace std;
+
+class Interface {
+    //Dados dois locais, indicar a melhor maneira de voar de um para o outro (menor número de voos).
+        //Local - aeroporto em especifico, cidade (considerar todos os aeroportos presentes) ou uma localização (coordenadas, considerar todos os aeroportas a X kms da localização).
+        //Rede de voos - ter em conta qualquer companhia aerea, somente uma ou um conjunto.
+        //usar formula de Haversine: hav(x) = sin^2(x/2)
+
+    //Informações sobre o aeroporto:
+        //quantos voos partem, quantas as companhias, para quantos destinos diferentes, de quantos paises diferentes, quantos aeroportos/cidades/países são atingíveis usando mas Y voos
+
+};
+
+
+#endif //PROJETOGRUPO2_INTERFACE_H
