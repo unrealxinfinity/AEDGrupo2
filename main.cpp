1454eb096b7cc12530f98dcda650f91e2aa04f28
HaoChang Fu <fuhaochang664961423@hotmail.com>
2022-12-28 03:49:01 +0000

    made some comments

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <unordered_set>
 
 //temp section for tests
+//Tested
 void testsAirportEqualAndAssignment(){
     Airport airportTest= Airport("BCN","BarcelonaAirport","Barcelona",1,1);
     Airport airportTest2= Airport("ABD","BarcelonaAirport","Barcelona",1,1);
@@ -14,6 +15,30 @@ void testsAirportEqualAndAssignment(){
     cout<<(airportTest2==airportTest)<<endl;
 
 }
+//error
+void airportSetTest(){
+    typedef unordered_set<Airport,AirportHash,AirportKeyEqual> Airports;
+    Airport a = Airport("Code1","Test","Uhm",1,2);
+    a.distanceSince=0;
+    a.visited=false;
+    a.addFlight(Flight("","",0));
+
+   /* Airport b = Airport("","Test","",0,0);
+    b.visited=false;
+    b.distanceSince=0;
+    b.addFlight(Flight("","",0));*/
+
+    Airport target = Airport("Code1","Test","Uhm",1,2);
+   // cout<<(a==b)<<endl;
+
+    Airports airports;
+    airports.insert(a);
+
+    cout<<airports.find(target)->getCode()<<endl;
+
+
+}
+//tested
 void testUnorderedSetFlights(){
     typedef unordered_set<Flight,FlightHash,FlightKeyEqual> Flights;
     Flights flights;
@@ -28,6 +53,6 @@ void testUnorderedSetFlights(){
 
 int main() {
 
-    testUnorderedSetFlights();
+    airportSetTest();
     return 0;
 }
