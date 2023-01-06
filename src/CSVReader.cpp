78aee4f09e2440c668bfa6a06b4953485cac0fdd
HaoChang Fu <fuhaochang664961423@hotmail.com>
2023-01-06 18:11:56 +0000

    changed citiesfrom/toAirport

--- src/CSVReader.cpp
+++ src/CSVReader.cpp
@@ -737,30 +737,31 @@ set<string> CSVReader::countriesToAirport(const std::string &cod) {
 /** Calculates the cities that are reachable by the outgoing flights in that airport
  * @attention Complexity : O(nlog(m)) (n = number of flights the airport has; m = number of distinct cities from airport)
  * @param cod Airport Code
- * @return set pair of city and country
+ * @return set of cities with pair of respective airportCode-country
  */
-set<pair<string,string>> CSVReader::citiesFromAirport(const std::string &cod) {
+set<pair<string,pair<string,string>>> CSVReader::citiesFromAirport(const std::string &cod) {
     list<Flight> temp= flightsFromAirport(cod);
-    set<pair<string,string>> res;
+    set<pair<string,pair<string,string>>> res;
     for(Flight f:temp){
         auto targetIt= airports.find(Airport(f.destAirportCode_));
         City *cityP=targetIt->getCity();
-        cities.insert({cityP->get_name(),cityP->get_country()});
+        res.insert({cityP->get_name(),{targetIt->getCode(),cityP->get_country()}});
     }
     return res;
 }
+
 /** Calculate the source cities of incoming flights to an airport
  * @attention Complexity : O(n + mlog(p)) (n = number of airports; m = number of flights to airport; p = number of distinct cities to airport)
  * @param cod : Airport code
- * @return set of pairs of city-country
+ * @return set of cities with pair of respective airportCode-country
  */
-set<pair<string,string>> CSVReader::citiesToAirport(const std::string &cod) {
+set<pair<string,pair<string,string>>> CSVReader::citiesToAirport(const std::string &cod) {
     list<pair<string, Flight>> flights = flightsToAirport(cod);
-    set<pair<string, string>> res;
+    set<pair<string,pair<string,string>>> res;
     for (const auto &p: flights) {
         auto targetIt = airports.find(Airport(p.first));
         City *cityP = targetIt->getCity();
-        res.insert({cityP->get_name(), cityP->get_country()});
+        res.insert({cityP->get_name(),{targetIt->getCode(),cityP->get_country()}});
     }
     return res;
 }
